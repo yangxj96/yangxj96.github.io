c8884e1c69db6b31a49562c7ec6b3c2d027fcd8a
道一 <yangxj96@gmail.com>
2022-08-17 11:36:31 +0800

    feat: 部署脚本

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+yarn run docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:yangxj96/yangxj96.github.io.git master
+#git push -f git@github.com:yangxj96/yangxj96.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:yangxj96/yangxj96.github.io.git master:gh-pages
 
 cd -
